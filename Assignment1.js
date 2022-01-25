1042ec1ad18b97835257bb4f34335b5921608a71
ShrutiGova <shrutigova@gmail.com>
2022-01-25 14:30:37 +0530

    Initial commit

---
+++ Assignment1.js
@@ -0,0 +1,25 @@
+// Assignment 1
+
+//variable declaration
+let Name = "Shruti";
+let age = 20;
+let email = "abcd@gmail.com";
+let phoneNo = 1234567890;
+let address = "Shajapur";
+let gender = "Female";
+
+console.log("Name:" + Name);
+console.log("Age:"+age);
+console.log("Email:" + email);
+console.log("Phone:" + phoneNo);
+console.log("Address:" +address);
+console.log("Gender:" + gender);
+
+//Output:
+// Name: Shruti
+//Age: 20
+//Email: abcd@gmail.com
+//Phone: 1234567890
+//Addres: Shajapur
+//Gender: Female
+
